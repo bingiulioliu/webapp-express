1c38f28134585d8dfa757bf86769cd57a5f874d0
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-06-10 22:25:31 +0200

    FEAT mostro tutte le recensioni nella index di /reviews

--- src/controllers/reviewsController.js
+++ src/controllers/reviewsController.js
@@ -3,9 +3,17 @@ import connection from "../db/connections/connection.js";
 
 async function index(request, response) {
     try{
+        const [rows] = await connection.query('select id, name, text_review, rating, title from reviews');
 
-    }catch{
-
+        response.json({
+            error: null,
+            results: rows
+        });
+    } catch(error) {
+        response.status(500).json({
+            error: 'Errore di caricamento',
+            results: []
+        })
     }
 };
 
